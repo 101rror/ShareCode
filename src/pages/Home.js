31ceb4030d2ca03674f00523dbdffab03238dab4
Mr. Error <mr.101rror@gmail.com>
2025-04-14 23:02:58 +0600

    Home Page Style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home Page</div>
+    <div className='homePageWrapper'>
+      <div className='formWrapper'>
+        <img src='/sharecode.png' alt='sharecode-logo'></img>
+        <h4 className='mainLabel'>Paste Invitation Room Id</h4>
+        <div className='inputGroup'>
+          <input type='text' className='inputBox' placeholder='ROOM ID' />
+          <input type='text' className='inputBox' placeholder='USERNAME' />
+          <button className='btn joinBtn'>Join</button>
+          <span className='createInfo'>
+            If you don't have an Invite then Create &nbsp;
+            <a href='' className='createNewBtn'>New Room</a>
+          </span>
+        </div>
+      </div>
+
+      <footer>
+        <h4>Built with ❣️ by <a href='https://github.com/101rror/'>Error</a></h4>
+      </footer>
+      
+    </div>
   )
 }
 
