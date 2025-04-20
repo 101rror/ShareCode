ed497a3d3102dd16ab3d18699e104f77eee385d0
Mr. Error <mr.101rror@gmail.com>
2025-04-20 20:43:53 +0600

    Editor page

---
+++ src/components/Editor.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+const Editor = () => {
+  useEffect(() => {
+    async function init() {
+      
+    }
+    init();
+  }, []);
+  
+  return <textarea id="realtimeEditor"></textarea>
+};
+
+export default Editor;
